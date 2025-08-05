65b3397abd8e048c5f4aec062d816c5d6201099d
Apeliemma <livingstoneapeli@gmail.com>
2025-08-05 03:12:04 +0300

    updated footer

--- public/constants.js
+++ public/constants.js
@@ -1,6 +1,6 @@
 // constants.js
 const API_ENDPOINTS = {
-    CONTACT: 'https://api.aurai.co.ke/contact'
+    CONTACT: 'https://api.aurai.co.ke/api/contact'
 };
 
 const API_KEYS = {
@@ -12,4 +12,9 @@ const APP_CONFIG = {
     APP_NAME: 'Aurai',
     APP_VERSION: '1.0.0',
     APP_ENV: 'production'
+};
+
+const ERROR_MESSAGES = {
+    GENERIC_ERROR: 'An unexpected error occurred. Please try again later.',
+    FORM_SUBMISSION_ERROR: 'There was an error submitting the form. Please check your input and try again.'
 };
