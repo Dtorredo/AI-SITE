981402073e01c8cf6d02f9d6fae9214804ebcbe0
Apeliemma <livingstoneapeli@gmail.com>
2025-08-05 03:04:09 +0300

    updated footer

--- public/constants.js
+++ public/constants.js
@@ -1,8 +1,15 @@
 // constants.js
 const API_ENDPOINTS = {
-    CONTACT: 'https://api.aurai.co.ke/api/contact'
+    CONTACT: 'https://api.aurai.co.ke/contact'
 };
 
 const API_KEYS = {
     FRONTEND: 'apelisoltechedgh45jfgdj48wifbiejiejiebc'
+};
+
+
+const APP_CONFIG = {
+    APP_NAME: 'Aurai',
+    APP_VERSION: '1.0.0',
+    APP_ENV: 'production'
 };
